850e0a2c2e9d383d6dd67b1b966788e6cabce9a2
Rennan555 <dungeonbr5@gmail.com>
2026-05-27 22:52:19 -0300

    feat: Initial Item class and entity added

---
+++ Scripts/Item.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class Item : Node2D
+{
+	// TODO
+	// [Export]
+	// private string updateName;
+	// private Update updateObject;
+	
+	public override void _Ready()
+	{
+	}
+	
+	public override void _Process(double delta)
+	{
+	}
+}
