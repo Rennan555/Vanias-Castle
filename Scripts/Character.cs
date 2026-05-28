6038291126912634a3c69e056e601d7fc8cb96b6
Rennan555 <dungeonbr5@gmail.com>
2026-05-27 22:23:47 -0300

    feat: Character class created as abstract

--- Scripts/Character.cs
+++ Scripts/Character.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class Character : CharacterBody2D
+public abstract partial class Character : CharacterBody2D
 {
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
