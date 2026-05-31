112c532eca5b332c0652157e80b98d3d5faa3496
Rennan555 <dungeonbr5@gmail.com>
2026-05-30 21:57:09 -0300

    feat: Base Scene entity added

--- Scripts/Props/Scene.cs
+++ Scripts/Props/Scene.cs
@@ -3,6 +3,15 @@ using System;
 
 public partial class Scene : Node2D
 {
+	private Node2D doorsNode;
+	private Godot.Collections.Array<Godot.Node> doors;
+	
+	public override void _Ready()
+	{
+		this.doorsNode = GetNode<Node2D>("DoorsNode");
+		this.doors = doorsNode.GetChildren();
+	}
+	
 	public override void _Process(double delta)
 	{
 		// Sair do jogo
